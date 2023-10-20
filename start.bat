0fb0bbdc2019b869a0af4b2493e4edc650b6bfb9
wwsuixin <wwsuixin@qq.com>
2023-10-20 14:49:54 +0800

    commit

--- start.bat
+++ start.bat
@@ -19,7 +19,7 @@ if not exist env\python311\python311.exe (
     )
     if exist tmp\python311.zip (
         echo 解压python311.zip
-        env\bandizip\Bandizip.x64.exe x -target:auto -o:env tmp\python311.zip
+        env\bandizip\Bandizip.x64.exe x -y  -target:auto -o:env tmp\python311.zip
         del tmp\python311.zip
     )
 )
@@ -37,11 +37,11 @@ if not exist env\PowerShell\pwsh.exe (
     )
     if exist tmp\PowerShell.zip (
         echo 解压PowerShell.zip
-        env\bandizip\Bandizip.x64.exe x -target:auto -o:env tmp\PowerShell.zip
+        env\bandizip\Bandizip.x64.exe x -y  -target:auto -o:env tmp\PowerShell.zip
         del tmp\PowerShell.zip
     )
 )
-if not exist env\java1.8\java.exe (
+if not exist env\java1.8\bin\java.exe (
     echo java1.8依赖环境不存在,开始下载java1.8.zip
     if not exist tmp\java1.8.zip (
         curl -L https://git.xfj0.cn/https://github.com/wwsuixin/ws_tools/releases/download/202310201112/jre1.8.zip -o tmp\java1.8.zip
@@ -54,12 +54,12 @@ if not exist env\java1.8\java.exe (
     )
     if exist tmp\java1.8.zip (
         echo 解压java1.8.zip
-        env\bandizip\Bandizip.x64.exe x -target:auto -o:env tmp\java1.8.zip
+        env\bandizip\Bandizip.x64.exe  x -y -target:auto -o:env tmp\java1.8.zip
         del tmp\java1.8.zip
     )
 )
 
-if not exist env\java17\java.exe (
+if not exist env\java17\bin\java.exe (
     echo java17依赖环境不存在,开始下载java17.zip
     if not exist tmp\java17.zip (
         curl -L https://git.xfj0.cn/https://github.com/wwsuixin/ws_tools/releases/download/202310201112/jre17.zip -o tmp\java17.zip
@@ -72,7 +72,7 @@ if not exist env\java17\java.exe (
     )
     if exist tmp\java17.zip (
         echo 解压java17.zip
-        env\bandizip\Bandizip.x64.exe x -target:auto -o:env tmp\java17.zip
+        env\bandizip\Bandizip.x64.exe x -y  -target:auto -o:env tmp\java17.zip
         del tmp\java17.zip
     )
 )
@@ -89,25 +89,18 @@ if not exist env\PortableGit\bin\git.exe (
     )
     if exist tmp\PortableGit.zip (
         echo 解压PortableGit.zip
-        env\bandizip\Bandizip.x64.exe x -target:auto -o:env tmp\PortableGit.zip
+        env\bandizip\Bandizip.x64.exe x -y  -target:auto -o:env tmp\PortableGit.zip
         del tmp\PortableGit.zip
     )
 )
 
 if not exist .venv\Scripts\python.exe (
-    set /p install_python=是否初始化环境[y/n]
-    if "%install_python%"=="y" (
-        echo 安装环境
-        env\Python311\python311.exe -m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
-        env\Python311\python311.exe -m pip install pdm 
-        env\Python311\python311.exe -m pdm config pypi.url https://mirrors.aliyun.com/pypi/simple/
-        env\Python311\python311.exe -m pdm config pypi.url https://mirrors.aliyun.com/pypi/simple/
-        env\Python311\python311.exe -m pdm install
-    ) else (
-        echo 不安装环境
-        pause
-        exit /b 1
-    )
+    echo 安装python依赖环境
+    env\Python311\python311.exe -m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+    env\Python311\python311.exe -m pip install pdm 
+    env\Python311\python311.exe -m pdm config pypi.url https://mirrors.aliyun.com/pypi/simple/
+    env\Python311\python311.exe -m pdm config pypi.url https://mirrors.aliyun.com/pypi/simple/
+    env\Python311\python311.exe -m pdm install
 )else (
     start http://127.0.0.1:60001
     .venv\Scripts\python.exe -m  flask -A start_web  run --host 0.0.0.0 --port 60001

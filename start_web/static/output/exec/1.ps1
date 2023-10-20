c8611f3d4a67a1ab15ce1ef87a5d4284f665fa61
wwsuixin <wwsuixin@qq.com>
2023-10-20 21:31:13 +0800

    commit

--- start_web/static/output/exec/1.ps1
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------------
-Set-PSDebug -Trace 1
-# 以下命令执行结束，请查看结果
-# ------------------------------------------------------------
-explorer C:\Users\Public\ws_tools\deployment_tools\oa;exit
-# ------------------------------------------------------------
-Get-Content 'C:\Users\Public\ws_tools\start_web\static\output\exec\1.ps1'  | Select-Object -Skip 2  | Select-Object -SkipLast 1

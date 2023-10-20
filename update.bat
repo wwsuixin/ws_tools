d92bb621ea4ed8904c4a89d4dca51ab60852ed5a
wwsuixin <wwsuixin@qq.com>
2023-10-20 22:09:44 +0800

    commit

--- update.bat
+++ update.bat
@@ -5,7 +5,10 @@ cd %~dp0
 echo 当前版本信息：
 type version.txt
 echo 获取github更新
-env\PortableGit\bin\git.exe  clone https://github.com/wwsuixin/ws_tools.git
+
+env\PortableGit\bin\git.exe pull https://github.com/wwsuixin/ws_tools.git
+
+
 echo 已更新到最新版本：
 type version.txt
 pause

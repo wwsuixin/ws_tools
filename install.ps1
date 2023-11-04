1f29284c87aae8e327aabb0495a341acecd59c82
wwsuixin <wwsuixin@qq.com>
2023-11-05 01:48:38 +0800

    commit

--- install.ps1
+++ install.ps1
@@ -1,144 +1,97 @@
+
 Set-Location -Path $PSScriptRoot
-if (-not (Test-Path -Path "tmp")) {
-    New-Item -ItemType Directory -Path "tmp"
+$tmp_dir_path = "tmp"
+if (-not (Test-Path -Path $tmp_dir_path)) {
+    New-Item -ItemType Directory -Path $tmp_dir_path
+}
+$env_dir_path = "env"
+if (-not (Test-Path -Path $env_dir_path)) {
+    New-Item -ItemType Directory -Path $env_dir_path
+}
+
+
+
+# 获取命令行传来的值，赋值到http_proxy
+if ($args.Length -eq 1) {
+    $http_proxy = $args[0]
 }
 
 
 
 
-if (-not (Test-Path -Path "env\python311\python311.exe")) {
-    Write-Host "python311依赖环境不存在,开始下载python311.zip"
-    if (-not (Test-Path -Path "tmp\python311.zip")) {
-        Start-Process -FilePath "env\aria2\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o tmp\python311.zip https://github.com/wwsuixin/ws_tools/releases/download/202310201112/python311.zip"
-        $process = Get-Process -Name "aria2c"
-        $process.WaitForExit()
-        if ((Get-Item "tmp\python311.zip").Length -eq 0) {
-            Write-Host "下载的文件大小为0KB，请检查网络问题"
-            Remove-Item -Path "tmp\python311.zip"
-            Read-Host -Prompt "Press Enter to exit"
-            exit 1
+function InstallTool($tool_name, $tmp_dir_path, $env_dir_path, $http_proxy) {
+    Write-Host "开始检测是否安装$tool_name..."
+    $tmp_tool_file_path = "$tmp_dir_path/$tool_name.zip"
+    $env_tool_dir_path = "$env_dir_path/$tool_name"
+    $env_tool_version_path = "$env_dir_path/$tool_name/w_version.txt"
+    $URL = "$http_proxy/https://github.com/wwsuixin/ws_tools/releases/download/latest/$tool_name.zip"
+    if ($tool_name -eq "start_web") {
+        $env_tool_version_path = "$tool_name/w_version.txt"
+    }
+    if ($tool_name -eq "venv") {
+        $env_tool_version_path = ".venv/w_version.txt"
+    }
+    if (-not (Test-Path -Path $env_tool_version_path)) {
+        Write-Host "[-] $tool_name 不存在,开始下载......"
+        # 使用powershell命令下载一个文件
+        if (-not (Test-Path -Path $tmp_tool_file_path)) {
+            if (-not (Test-Path -Path "env\aria2c\aria2c.exe")) {
+                # 使用Invoke-WebRequest cmdlet下载文件
+                (New-Object System.Net.WebClient).DownloadFile($URL, $tmp_tool_file_path)
+            }
+            else {
+                Start-Process -FilePath "env\aria2c\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o $tmp_tool_file_path $URL"
+                $process = Get-Process -Name "aria2c"
+                $process.WaitForExit()
+            }
+            
+            #判断文件不存在或大小为0KB
+            if (-not (Test-Path -Path $tmp_tool_file_path)) {
+                Write-Host "[-] $tool_name.zip下载失败，请检查网络问题"
+                Read-Host -Prompt "请按任意键继续. . ."
+                exit 1
+            }
+            #判断文件大小为0KB
+            if ((Get-Item $tmp_tool_file_path).Length -eq 0) {
+                Write-Host "[-] 下载的文件大小为0KB，请检查网络问题"
+                Read-Host -Prompt "请按任意键继续. . ."
+                Remove-Item -Path $tmp_tool_file_path
+                exit 1
+            }
+        }
+        # 解压缩文件
+        if (-not (Test-Path -Path "env\bandizip\bandizip.exe")) {
+            Expand-Archive -Path $tmp_tool_file_path -DestinationPath "$env_tool_dir_path/"
+        }
+        else {
+            if ($tool_name -eq "start_web") {
+                Start-Process -FilePath "env\bandizip\bandizip.exe" -ArgumentList "x -y  -o:./ $tmp_tool_file_path"
+            }elseif ($tool_name -eq "venv") {
+                Start-Process -FilePath "env\bandizip\bandizip.exe" -ArgumentList "x -y  -o:./.venv $tmp_tool_file_path"
+            }
+            else {
+                Start-Process -FilePath "env\bandizip\bandizip.exe" -ArgumentList "x -y  -target:auto -o:$env_dir_path $tmp_tool_file_path"
+            }
+            $process = Get-Process -Name "bandizip"
+            $process.WaitForExit()
         }
 
-    }
-    if (Test-Path -Path "tmp\python311.zip") {
-        Write-Host "解压python311.zip"
-        Start-Process -FilePath "env\bandizip\Bandizip.x64.exe" -ArgumentList "x -y  -target:auto -o:env tmp\python311.zip"
-        $process = Get-Process -Name "Bandizip.x64"
-        $process.WaitForExit()
-        Remove-Item -Path "tmp\python311.zip"
-    }
-}
-
-if (-not (Test-Path -Path "env\PowerShell\pwsh.exe")) {
-    Write-Host "PowerShell依赖环境不存在,开始下载PowerShell.zip"
-    if (-not (Test-Path -Path "tmp\PowerShell.zip")) {
-        Start-Process -FilePath "env\aria2\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o tmp\PowerShell.zip https://github.com/wwsuixin/ws_tools/releases/download/202310201112/PowerShell.zip"
-        $process = Get-Process -Name "aria2c"
-        $process.WaitForExit()
-        if ((Get-Item "tmp\PowerShell.zip").Length -eq 0) {
-            Write-Host "下载的文件大小为0KB，请检查网络问题"
-            Remove-Item -Path "tmp\PowerShell.zip"
-            Read-Host -Prompt "Press Enter to exit"
+        if (-not (Test-Path -Path $env_tool_version_path)) {
+            Write-Host "[-] $tool_name.zip解压失败，请检查"
+            Read-Host -Prompt "请按任意键继续. . ."
             exit 1
         }
-    }
-    if (Test-Path -Path "tmp\PowerShell.zip") {
-        Write-Host "解压PowerShell.zip"
-        Start-Process -FilePath "env\bandizip\Bandizip.x64.exe" -ArgumentList "x -y  -target:auto -o:env tmp\PowerShell.zip"
-        $process = Get-Process -Name "Bandizip.x64"
-        $process.WaitForExit()
-        Remove-Item -Path "tmp\PowerShell.zip"
-    }
-}
-if (-not (Test-Path -Path "env\java1.8\bin\java.exe")) {
-    Write-Host "java1.8依赖环境不存在,开始下载java1.8.zip"
-    if (-not (Test-Path -Path "tmp\java1.8.zip")) {
-        Start-Process -FilePath "env\aria2\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o tmp\java1.8.zip https://github.com/wwsuixin/ws_tools/releases/download/202310201112/java1.8.zip"
-        $process = Get-Process -Name "aria2c"
-        $process.WaitForExit()
-        if ((Get-Item "tmp\java1.8.zip").Length -eq 0) {
-            Write-Host "下载的文件大小为0KB，请检查网络问题"
-            Remove-Item -Path "tmp\java1.8.zip"
-            Read-Host -Prompt "Press Enter to exit"
-            exit 1
-        }
-    }
-    if (Test-Path -Path "tmp\java1.8.zip") {
-        Write-Host "解压java1.8.zip"
-        Start-Process -FilePath "env\bandizip\Bandizip.x64.exe" -ArgumentList "x -y -target:auto -o:env tmp\java1.8.zip"
-        $process = Get-Process -Name "Bandizip.x64"
-        $process.WaitForExit()
-        Remove-Item -Path "tmp\java1.8.zip"
-    }
-}
+        Write-Host "[+] 安装完成$tool_name"
 
-if (-not (Test-Path -Path "env\java17\bin\java.exe")) {
-    Write-Host "java17依赖环境不存在,开始下载java17.zip"
-    if (-not (Test-Path -Path "tmp\java17.zip")) {
-        Start-Process -FilePath "env\aria2\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o tmp\java17.zip https://github.com/wwsuixin/ws_tools/releases/download/202310201112/java17.zip"
-        $process = Get-Process -Name "aria2c"
-        $process.WaitForExit()
-        if ((Get-Item "tmp\java17.zip").Length -eq 0) {
-            Write-Host "下载的文件大小为0KB，请检查网络问题"
-            Remove-Item -Path "tmp\java17.zip"
-            Read-Host -Prompt "Press Enter to exit"
-            exit 1
-        }
+        Remove-Item -Path $tmp_tool_file_path
     }
-    if (Test-Path -Path "tmp\java17.zip") {
-        Write-Host "解压java17.zip"
-        Start-Process -FilePath "env\bandizip\Bandizip.x64.exe" -ArgumentList "x -y  -target:auto -o:env tmp\java17.zip"
-        $process = Get-Process -Name "Bandizip.x64"
-        $process.WaitForExit()
-        Remove-Item -Path "tmp\java17.zip"
-    }
-}
-if (-not (Test-Path -Path "env\PortableGit\bin\git.exe")) {
-    Write-Host "git依赖环境不存在,开始下载PortableGit.zip"
-    if (-not (Test-Path -Path "tmp\PortableGit.zip")) {
-        Start-Process -FilePath "env\aria2\aria2c.exe" -ArgumentList "-x 16 -s 16 -k 1M -o tmp\PortableGit.zip https://github.com/wwsuixin/ws_tools/releases/download/202310201112/PortableGit.zip"
-        $process = Get-Process -Name "aria2c"
-        $process.WaitForExit()
-        if ((Get-Item "tmp\PortableGit.zip").Length -eq 0) {
-            Write-Host "下载的文件大小为0KB，请检查网络问题"
-            Remove-Item -Path "tmp\PortableGit.zip"
-            Read-Host -Prompt "Press Enter to exit"
-            exit 1
-        }
-    }
-    if (Test-Path -Path "tmp\PortableGit.zip") {
-        Write-Host "解压PortableGit.zip"
-        Start-Process -FilePath "env\bandizip\Bandizip.x64.exe" -ArgumentList "x -y  -target:auto -o:env tmp\PortableGit.zip"
-        $process = Get-Process -Name "Bandizip.x64"
-        $process.WaitForExit()
-        Remove-Item -Path "tmp\PortableGit.zip"
-    }
-}
-
-if (-not (Test-Path -Path ".venv\Scripts\python.exe")) {
-    Write-Host "安装python依赖环境"
-    Start-Process -FilePath "env\Python311\python311.exe" -ArgumentList "-m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/"
-    $process = Start-Process -FilePath "env\Python311\python311.exe" -ArgumentList "-m pip install pdm" -PassThru
-    $process.WaitForExit()
-    if ($process.ExitCode -ne 0) {
-        Write-Host "pdm安装失败"
-        Read-Host -Prompt "按Enter键退出"
-        exit 1
-    }
-    Start-Process -FilePath "env\Python311\python311.exe" -ArgumentList "-m pdm config pypi.url https://mirrors.aliyun.com/pypi/simple/"
-    $process = Start-Process -FilePath "env\Python311\python311.exe" -ArgumentList "-m pdm install" -PassThru
-    $process.WaitForExit()
-    if ($process.ExitCode -ne 0) {
-        Write-Host "pdm install命令执行失败"
-        Read-Host -Prompt "按Enter键退出"
-        exit 1
-    }
+    Write-Host "[+] 已安装$tool_name..."
 
 }
 
-    Write-Host "安装完成，请重新运行start.bat"
-
-
-Read-Host -Prompt "Press Enter to exit"
-
+InstallTool "aria2c" $tmp_dir_path $env_dir_path $http_proxy
+InstallTool "bandizip" $tmp_dir_path $env_dir_path $http_proxy
+InstallTool "python311" $tmp_dir_path $env_dir_path $http_proxy
+InstallTool "start_web" $tmp_dir_path $env_dir_path $http_proxy
+InstallTool "venv" $tmp_dir_path $env_dir_path $http_proxy
 
--- start.bat
+++ start.bat
@@ -1,14 +1,45 @@
 @echo off 
-title ws_tools_Æô¶¯´°¿Ú
 
+setlocal enabledelayedexpansion
+title ws_tools
 
+@REM ???????
+cd /d %~dp0
+
+@REM ????url
+set http_proxy=https://gh.con.sh
+
+:func
 if exist .venv\Scripts\python.exe (
-    start "http://127.0.0.1:60001"
+    start chrome "http://127.0.0.1:60001"
     cmd /c  ".venv\Scripts\python.exe -m  flask -A start_web  run --host 0.0.0.0 --port 60001"
-
+    exit
 ) else (
-    powershell -ExecutionPolicy Bypass -File install.ps1
+    @REM ?while??????
+
+    if not exist install.ps1 (
+        set /p yn=[-]???????????,??????????[y/n]:
+        @REM ??????
+        if  not "!yn!" == "y" (
+            echo [-] ?????
+            pause
+            exit
+        )
+        echo [+] ????????
+        powershell -ExecutionPolicy Bypass -Command "& {iwr -useb !http_proxy!/https://github.com/wwsuixin/ws_tools/raw/main/install.ps1 -OutFile install.ps1}"
+
+    ) else (
+        set /p yn=[+]???????,??????[y/n]:
+        if  not "!yn!" == "y" (
+            echo [-] ?????
+            pause
+            exit
+        )
+        echo [+] ????
+        powershell -ExecutionPolicy Bypass -File install.ps1 !http_proxy!
+
+    )
 )
+goto func
 
-
-
+pause

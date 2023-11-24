4d1562516e3b3f90a9731e0a765d18b737c6f665
wwsuixin <wwsuixin@qq.com>
2023-11-24 23:49:59 +0800

    commit

--- start.bat
+++ start.bat
@@ -2,37 +2,40 @@
 title ws_tools
 setlocal enabledelayedexpansion
 
-@REM 切换到当前目录
+@REM Switch to the current directory
 cd /d %~dp0
 
-@REM 设置代理url
+@REM Set proxy URL
 set http_proxy=https://gh.con.sh
+@REM set http_proxy=https://download.nuaa.cf
+@REM set http_proxy=https://download.njuu.cf
+@REM set http_proxy=https://dl-slb.ghpig.top
+@REM set http_proxy=https://mirror.ghproxy.com
+@REM set http_proxy=https://gh-proxy.com
+@REM set http_proxy=https://ghproxy.net
+
 if not exist tmp (
     mkdir tmp
 )
 
 if not exist w_version.txt (
-    echo [-]检测到当前环境还未安装, 开始下载运行脚本
-    choice /C YN /M "询问是否开始安装Y继续，回N退出:"
-    if errorlevel 2 (
-        echo [-]安装已取消
-        exit
-    )
-    echo [-]开始下载运行脚本
+    echo [-]Detected that the current environment is not installed, start downloading and running the script
     powershell -ExecutionPolicy Bypass -Command "& {iwr -useb !http_proxy!/https://github.com/wwsuixin/ws_tools/releases/download/main/ws_tools.zip -OutFile tmp/ws_tools.zip}"
-    @REM 解压ws_tools.zip到指定文件夹
-   @REM 覆盖解压ws_tools.zip到指定文件夹
-   powershell -ExecutionPolicy Bypass -Command "Expand-Archive -Path 'tmp\ws_tools.zip' -DestinationPath './' -Force"
+    @REM Extract ws_tools.zip to the specified folder
+    @REM Overwrite and extract ws_tools.zip to the specified folder
+    powershell -ExecutionPolicy Bypass -Command "Expand-Archive -Path 'tmp\ws_tools.zip' -DestinationPath './' -Force"
 )
 
 if not exist w_version.txt (
-    echo [-]网络环境异常,请检查网络......
+    echo [-]Network environment abnormal, please check the network......
+    echo [!]Please manually modify the ninth line of the script, replacing http_proxy with another proxy URL
     pause
     exit
 )
-@REM 删除tmp/ws_tools.zip
+@REM Delete tmp/ws_tools.zip
 if exist tmp\ws_tools.zip (
     del tmp\ws_tools.zip
 )
 
-powershell -ExecutionPolicy Bypass -File run.ps1 !http_proxy!
+env\powershell\pwsh.exe  -ExecutionPolicy Bypass -File run.ps1 !http_proxy!
+exit

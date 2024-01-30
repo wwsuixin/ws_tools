f4a72fe238f898e34649e954a3e3e784433dd494
wwsuixin <wwsuixin@qq.com>
2024-01-30 17:22:29 +0800

    commit

--- start.bat
+++ start.bat
@@ -6,13 +6,36 @@ setlocal enabledelayedexpansion
 cd /d %~dp0
 
 @REM Set proxy URL
-set github_proxy=https://gh.con.sh/
-@REM set http_proxy=https://download.nuaa.cf/
-@REM set http_proxy=https://download.njuu.cf/
-@REM set http_proxy=https://dl-slb.ghpig.top/
-@REM set http_proxy=https://mirror.ghproxy.com/
-@REM set http_proxy=https://gh-proxy.com/
-@REM set http_proxy=https://ghproxy.net/
+set github_proxy=
+echo ---------------------------------------------------
+echo 1: https://gh.con.sh/ (recommend)
+echo 2: https://gh.ddlc.top/
+echo 3: https://dl.ghpig.top/
+echo 4: https://slink.ltd/
+echo 5: https://git.xfj0.cn/
+echo 6: https://gh.h233.eu.org/
+echo 7: https://ghps.cc/
+echo 8: https://gh-proxy.com/
+echo 9: https://hub.gitmirror.com/
+set /p user_input=Please enter a number from 1 to 9(default 1): 
+if "%user_input%"=="" set user_input=1
+
+if %user_input%==1 set github_proxy=https://gh.con.sh/
+if %user_input%==2 set github_proxy=https://gh.ddlc.top/
+if %user_input%==3 set github_proxy=https://dl.ghpig.top/
+if %user_input%==4 set github_proxy=https://slink.ltd/
+if %user_input%==5 set github_proxy=https://git.xfj0.cn/
+if %user_input%==6 set github_proxy=https://gh.h233.eu.org/
+if %user_input%==7 set github_proxy=https://ghps.cc/
+if %user_input%==8 set github_proxy=https://gh-proxy.com/
+if %user_input%==9 set github_proxy=https://hub.gitmirror.com/
+
+
+echo The agent you have chosen is !github_proxy!
+
+echo ---------------------------------------------------
+
+
 
 if not exist tmp (
     mkdir tmp
